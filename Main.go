97ce571f1a6fffe8f54ffc10c47f3e9be35d281d
daniel <daniel@chopup.me>
2018-06-11 06:08:50 +0100

    initial commit

---
+++ Main.go
@@ -0,0 +1,219 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	"gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
+)
+
+func ErrorWithJSON(w http.ResponseWriter, message string, code int) {
+	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+	w.WriteHeader(code)
+	fmt.Fprintf(w, "{message: %q}", message)
+}
+
+func ResponseWithJSON(w http.ResponseWriter, json []byte, code int) {
+	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+	w.WriteHeader(code)
+	w.Write(json)
+}
+
+type Book struct {
+	ISBN    string   `json:"isbn"`
+	Title   string   `json:"title"`
+	Authors []string `json:"authors"`
+	Price   string   `json:"price"`
+}
+
+func main() {
+	session, err := mgo.Dial("localhost")
+	if err != nil {
+		panic(err)
+	}
+	defer session.Close()
+
+	session.SetMode(mgo.Monotonic, true)
+	ensureIndex(session)
+
+	r := mux.NewRouter()
+	r.HandleFunc("/books", allBooks(session)).Methods("GET")
+	r.HandleFunc("/books", addBook(session)).Methods("POST")
+	r.HandleFunc("/books/{isbn}", bookByISBN(session)).Methods("GET")
+	r.HandleFunc("/books/{isbn}", updateBook(session)).Methods("PUT")
+	r.HandleFunc("/books/{isbn}", deleteBook(session)).Methods("DELETE")
+	if err := http.ListenAndServe(":3000", r); err != nil {
+        log.Fatal(err)
+    }
+}
+
+func ensureIndex(s *mgo.Session) {
+	session := s.Copy()
+	defer session.Close()
+
+	c := session.DB("store").C("books")
+
+	index := mgo.Index{
+		Key:        []string{"isbn"},
+		Unique:     true,
+		DropDups:   true,
+		Background: true,
+		Sparse:     true,
+	}
+	err := c.EnsureIndex(index)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func allBooks(s *mgo.Session) func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		session := s.Copy()
+		defer session.Close()
+
+		c := session.DB("store").C("books")
+
+		var books []Book
+		err := c.Find(bson.M{}).All(&books)
+		if err != nil {
+			ErrorWithJSON(w, "Database error", http.StatusInternalServerError)
+			log.Println("Failed get all books: ", err)
+			return
+		}
+
+		respBody, err := json.MarshalIndent(books, "", "  ")
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		ResponseWithJSON(w, respBody, http.StatusOK)
+	}
+}
+
+func addBook(s *mgo.Session) func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		session := s.Copy()
+		defer session.Close()
+
+		var book Book
+		decoder := json.NewDecoder(r.Body)
+		err := decoder.Decode(&book)
+		if err != nil {
+			ErrorWithJSON(w, "Incorrect body", http.StatusBadRequest)
+			return
+		}
+
+		c := session.DB("store").C("books")
+
+		err = c.Insert(book)
+		if err != nil {
+			if mgo.IsDup(err) {
+				ErrorWithJSON(w, "Book with this ISBN already exists", http.StatusBadRequest)
+				return
+			}
+
+			ErrorWithJSON(w, "Database error", http.StatusInternalServerError)
+			log.Println("Failed insert book: ", err)
+			return
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		w.Header().Set("Location", r.URL.Path+"/"+book.ISBN)
+		w.WriteHeader(http.StatusCreated)
+	}
+}
+
+func bookByISBN(s *mgo.Session) func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		session := s.Copy()
+		defer session.Close()
+
+		params := mux.Vars(r)
+
+		c := session.DB("store").C("books")
+
+		var book Book
+		err := c.Find(bson.M{"isbn": params["isbn"]}).One(&book)
+		if err != nil {
+			ErrorWithJSON(w, "Database error", http.StatusInternalServerError)
+			log.Println("Failed find book: ", err)
+			return
+		}
+
+		if book.ISBN == "" {
+			ErrorWithJSON(w, "Book not found", http.StatusNotFound)
+			return
+		}
+
+		respBody, err := json.MarshalIndent(book, "", "  ")
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		ResponseWithJSON(w, respBody, http.StatusOK)
+	}
+}
+
+func updateBook(s *mgo.Session) func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		session := s.Copy()
+		defer session.Close()
+
+		params := mux.Vars(r)
+
+		var book Book
+		decoder := json.NewDecoder(r.Body)
+		err := decoder.Decode(&book)
+		if err != nil {
+			ErrorWithJSON(w, "Incorrect body", http.StatusBadRequest)
+			return
+		}
+
+		c := session.DB("store").C("books")
+
+		err = c.Update(bson.M{"isbn": params["isbn"]}, &book)
+		if err != nil {
+			switch err {
+			default:
+				ErrorWithJSON(w, "Database error", http.StatusInternalServerError)
+				log.Println("Failed update book: ", err)
+				return
+			case mgo.ErrNotFound:
+				ErrorWithJSON(w, "Book not found", http.StatusNotFound)
+				return
+			}
+		}
+
+		w.WriteHeader(http.StatusNoContent)
+	}
+}
+
+func deleteBook(s *mgo.Session) func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		session := s.Copy()
+		defer session.Close()
+
+		params := mux.Vars(r)
+
+		c := session.DB("store").C("books")
+
+		err := c.Remove(bson.M{"isbn": params["isbn"]})
+		if err != nil {
+			switch err {
+			default:
+				ErrorWithJSON(w, "Database error", http.StatusInternalServerError)
+				log.Println("Failed delete book: ", err)
+				return
+			case mgo.ErrNotFound:
+				ErrorWithJSON(w, "Book not found", http.StatusNotFound)
+				return
+			}
+		}
+
+		w.WriteHeader(http.StatusNoContent)
+	}
+}
